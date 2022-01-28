18a0a66f8ed2f76193807e513c966408d37fe0e9
Javier Álvarez Liébana <javalv09@ucm.es>
2022-01-29 00:00:27 +0100

    .

--- scripts/script21.R
+++ scripts/script21.R
@@ -1,125 +1,351 @@
-# ----- tidyverse: profundizando  -----
+# ----- intro a ggplot2 -----
 
-# ----- elecciones ----- 
-
-# leemos datos de wikipedia
-library(rvest)
 library(tidyverse)
-wiki <-
-  paste0("https://en.wikipedia.org/wiki/Opinion_polling_for_the_next_Spanish_general_election")
 
-# Leemos html
-html <- read_html(wiki)
-# Seleccionamos las tablas del HTML
-tablas <- html_elements(html, ".wikitable")
+# Primer intento: scatter plot o diagrama de puntos
 
-# Obtenemos las dos primeras tablas: encuestas de 2022 y 2021
-encuestas_2022 <- html_table(tablas[[1]])
-encuestas_2022
-encuestas_2021 <- html_table(tablas[[2]])
-encuestas_2021
+### Datos: gapminder
+library(gapminder)
+gapminder
+glimpse(gapminder)
 
-# Renombramos columnas
-nombre_cols <-
-  c("casa", "fechas", "muestra", "participacion", "PSOE", "PP",	"Vox",
-    "UP", "Cs", "ERC", "MP", "JxCat",	"PNV",	"EHBildu", "CUP",
-    "CC", "BNG", "NA+", "PRC", "EV", "ventaja")
-names(encuestas_2022) <- names(encuestas_2021) <- nombre_cols
-encuestas_2022
-encuestas_2021
+# Filtraremos los datos de 1997
+gapminder_1997 <- gapminder %>% filter(year == 1997)
+gapminder_1997
 
-# Eliminar filas
-encuestas_2022 <- encuestas_2022 %>% slice(-1)
-encuestas_2022
-encuestas_2021 <- encuestas_2021 %>% slice(-1)
-encuestas_2021
+# Diagrama de puntos
+ggplot(gapminder_1997, aes(x = gdpPercap, y = pop)) +
+  geom_point()
+
+### Mapeado de elementos (aesthetics)
+ggplot(gapminder_1997, aes(y = gdpPercap, x = pop)) +
+  geom_point()
+
+ggplot(gapminder_1997, aes(y = gdpPercap, x = lifeExp)) +
+  geom_point()
+
+# Color con palabra reservada
+ggplot(gapminder_1997, aes(y = gdpPercap, x = lifeExp)) +
+  geom_point(color = "red")
+
+# Color en hexadecimal, de la página https://htmlcolorcodes.com/es/
+ggplot(gapminder_1997, aes(y = gdpPercap, x = lifeExp)) +
+  geom_point(color = "#2EA2D8")
+
+# Color opaco
+ggplot(gapminder_1997, aes(y = gdpPercap, x = lifeExp)) +
+  geom_point(color = "#A02B85", size = 4)
+
+# alpha = 50%
+ggplot(gapminder_1997, aes(y = gdpPercap, x = lifeExp)) +
+  geom_point(color = "#A02B85", size = 8, alpha = 0.4)
+
+# mapear en aes() para que dependan de los datos, por ejemplo,
+# asignándole un color a cada dato en función de su continente
+ggplot(gapminder_1997, aes(y = gdpPercap, x = lifeExp, color = continent)) +
+  geom_point(size = 4.5)
+
+# combinarlo con lo que hemos hecho anteriormente e indicarle 
+# además que queremos el tamaño en función de la población,
+# con cierto grado de transparencia. 
+ggplot(gapminder_1997,
+       aes(y = gdpPercap, x = lifeExp, color = continent, size = pop)) +
+  geom_point(alpha = 0.7)
+
+# añadir las variables en función de la forma de la geometría
+# (en este caso la forma de los «puntos») con `shape = ...`,
+# haciéndola depender de `continent` 
+ggplot(gapminder_1997,
+       aes(y = gdpPercap, x = lifeExp, shape = continent, size = pop)) +
+  geom_point(alpha = 0.7)
+
+### Scales y paletas de colores
+ggplot(gapminder_1997,
+       aes(y = gdpPercap, x = lifeExp,
+           color = continent, size = pop)) +
+  geom_point(alpha = 0.7)
+
+# ejes escala logarítmica
+ggplot(gapminder_1997,
+       aes(y = gdpPercap, x = lifeExp,
+           color = continent, size = pop)) +
+  geom_point(alpha = 0.7) +
+  # Eje Y con escala logarítmica
+  scale_y_log10()
+
+# escala colores manual
+ggplot(gapminder_1997,
+       aes(y = gdpPercap, x = lifeExp,
+           color = continent, size = pop)) +
+  geom_point(alpha = 0.7) +
+  # Eje Y con escala logarítmica
+  scale_y_log10() +
+  # Escala manual de colores
+  scale_color_manual(values = c("#A02B85", "#2DE86B", "#4FB2CA",
+                                "#E8DA2D", "#E84C2D"))
+
+# paletas
+library(ggthemes)
+
+# scale_color_economist()
+ggplot(gapminder_1997,
+       aes(y = gdpPercap, x = lifeExp,
+           color = continent, size = pop)) +
+  geom_point(alpha = 0.8) +
+  # Eje Y con escala logarítmica
+  scale_y_log10() +
+  scale_color_economist()
+
+# scale_color_excel()
+ggplot(gapminder_1997,
+       aes(y = gdpPercap, x = lifeExp,
+           color = continent, size = pop)) +
+  geom_point(alpha = 0.8) +
+  # Eje Y con escala logarítmica
+  scale_y_log10() +
+  scale_color_excel()
+
+# scale_color_tableau()
+ggplot(gapminder_1997,
+       aes(y = gdpPercap, x = lifeExp,
+           color = continent, size = pop)) +
+  geom_point(alpha = 0.8) +
+  # Eje Y con escala logarítmica
+  scale_y_log10() +
+  scale_color_tableau()
+
+devtools::install_github(repo = "https://github.com/aljrico/harrypotter")
+library(harrypotter)
+ggplot(gapminder_1997,
+       aes(y = gdpPercap, x = lifeExp,
+           color = continent, size = pop)) +
+  geom_point(alpha = 0.9) +
+  scale_y_log10() +
+  scale_color_hp_d(option = "gryffindor")
+
+ggplot(gapminder_1997,
+       aes(y = gdpPercap, x = lifeExp,
+           color = continent, size = pop)) +
+  geom_point(alpha = 0.9) +
+  scale_y_log10() +
+  scale_color_hp_d(option = "hufflepuff")
 
 
-# Añadir columna de año
-encuestas_2022 <- encuestas_2022 %>% mutate(anno = 2022)
-encuestas_2021 <- encuestas_2021 %>% mutate(anno = 2021)
+devtools::install_github("G-Thomson/Manu")
+library(Manu)
+ggplot(gapminder_1997,
+       aes(y = gdpPercap, x = lifeExp,
+           color = continent, size = pop)) +
+  geom_point(alpha = 0.9) +
+  scale_y_log10() +
+  # paleta del pájaro Takahē - Porphyrio hochstetteri
+  scale_colour_manual(values = get_pal("Takahe"))
 
-# Juntar tablas
-encuestas <- rbind(encuestas_2022, encuestas_2021)
+devtools::install_github("BlakeRMills/MetBrewer") 
+library(MetBrewer)
+MetBrewer::met.brewer("Renoir")
+MetBrewer::met.brewer("Monet")
+MetBrewer::met.brewer("Hokusai")
 
-# Convertir a numéricas
-encuestas_depurado <-
-  encuestas %>%
-  # Quitamos "," como millares en números
-  mutate(muestra = gsub("?", "", gsub(",", "", muestra)),
-         participacion = gsub("?", NA, participacion)) %>%
-  mutate_at(vars(!contains(c("casa", "fechas"))), as.numeric)
-encuestas_depurado
+ggplot(gapminder_1997,
+       aes(y = gdpPercap, x = lifeExp,
+           color = continent, size = pop)) +
+  geom_point(alpha = 0.9) +
+  scale_y_log10() +
+  scale_colour_manual(values = met.brewer("Klimt"))
 
-# Convertir las fechas de campo
-fechas_intermedias <- str_split(encuestas_depurado$fechas, "–")
-fechas_intermedias <-
-  map_dfr(fechas_intermedias,
-          function(x) { tibble("fecha_inicio" = x[1],
-                               "fecha_final" = x[2]) })
-fechas_intermedias <-
-  fechas_intermedias %>% 
-  mutate(fecha_final = ifelse(is.na(fecha_final),
-                              fecha_inicio, fecha_final))
-fechas_intermedias <-
-  fechas_intermedias %>% 
-  mutate(fecha_inicio =
-           ifelse(nchar(fecha_inicio) <= 2,
-                  paste(fecha_inicio,
-                        paste0(rev(rev(unlist(str_split(fecha_final, "")))[1:3]),
-                               collapse = "")), fecha_inicio))
-library(lubridate)
-encuestas_depurado <-
-  bind_cols(encuestas_depurado,
-            fechas_intermedias) %>%
-  mutate(fecha_inicio = dmy(paste(fecha_inicio, anno)),
-         fecha_final = dmy(paste(fecha_final, anno))) %>%
-  select(-c(fechas, anno, fecha_inicio)) %>%
-  relocate(fecha_final, .after = casa)
-encuestas_depurado
+### Geometrías (geom)
 
-# Limpiamos nombres de encuestas
-encuestas_depurado <-
-  encuestas_depurado %>%
-  # Limpiamos nombre encuestas
-  mutate(casa = toupper(map_chr(str_split(casa, "\\["),
-                                function (y) { y[1] } )))
-# Consultas
+# Sin separar por continente
+ggplot(gapminder_1997,
+       aes(y = gdpPercap, x = lifeExp)) +
+  geom_line(alpha = 0.8) +
+  scale_y_log10() +
+  scale_color_tableau()
 
-# ¿Cuáles son las 10 encuestas con mayor tamaño muestral?
-encuestas_depurado %>% slice_max(muestra, n = 10)
+# Separando por continente
+ggplot(gapminder_1997,
+       aes(y = gdpPercap, x = lifeExp,
+           color = continent)) +
+  geom_line(alpha = 0.8) +
+  scale_y_log10() +
+  scale_color_tableau()
 
-# ¿Cuáles son las encuestas más recientes?
-encuestas_depurado %>% arrange(desc(fecha_final))
+# geom_hex
+ggplot(gapminder_1997,
+       aes(y = gdpPercap, x = lifeExp,
+           fill = continent, size = pop)) +
+  geom_hex(alpha = 0.8) +
+  scale_y_log10() + scale_fill_tableau()
 
-# ¿Cuáles son las 5 encuestas en las que el PSOE tiene mayor proyección?
-encuestas_depurado %>% slice_max(PSOE, n = 5)
+# geom_tile()
+ggplot(gapminder_1997,
+       aes(y = gdpPercap, x = lifeExp,
+           color = continent, size = pop)) +
+  geom_tile(alpha = 0.8) +
+  scale_y_log10() +
+  scale_color_tableau()
 
-# ¿Cuál es la encuesta del CIS en la que el PP tiene mayor proyección?
-encuestas_depurado %>%
-  filter(casa == "CIS") %>%
-  slice_max(PP, n = 1)
+# geom_text()
+ggplot(gapminder_1997,
+       aes(y = gdpPercap, x = lifeExp,
+           color = continent, size = pop, label = country)) +
+  geom_text(alpha = 0.8) +
+  scale_y_log10() +
+  scale_color_tableau()
 
-# ¿Cuál es el promedio de las encuestas del PSOE y PP por casa encuestadora?
-encuestas_depurado %>%
-  select(-fecha_final) %>%
-  group_by(casa) %>%
-  summarise(media_PSOE = mean(PSOE), media_PP = mean(PP)) %>%
-  ungroup()
+### Componer (facets)
 
-# ¿Cuál es son las 3 casas encuestadoras más sesgada hacia el PSOE
-# (con mayor diferencia de promedio de PSOE vs PP)? ¿Y hacia el PP?
-encuestas_depurado %>%
-  select(-fecha_final) %>%
-  group_by(casa) %>%
-  summarise(media_PSOE = mean(PSOE), media_PP = mean(PP)) %>%
-  ungroup() %>%
-  mutate(diferencia = media_PSOE - media_PP) %>%
-  slice_max(diferencia, n = 3)
-encuestas_depurado %>%
-  select(-fecha_final) %>%
-  group_by(casa) %>%
-  summarise(media_PSOE = mean(PSOE), media_PP = mean(PP)) %>%
-  ungroup() %>%
-  mutate(diferencia = media_PP - media_PSOE) %>%
-  slice_max(diferencia, n = 3)
+ggplot(gapminder_1997,
+       aes(y = gdpPercap, x = lifeExp)) +
+  geom_point(alpha = 0.9) +
+  scale_y_log10() +
+  facet_wrap(~ continent)
+
+ggplot(gapminder_1997,
+       aes(y = gdpPercap, x = lifeExp)) +
+  geom_point(alpha = 0.9) +
+  scale_y_log10() +
+  facet_wrap(~ continent, nrow = 3)
+
+ggplot(gapminder_1997,
+       aes(y = gdpPercap, x = lifeExp)) +
+  geom_point(alpha = 0.9) +
+  scale_y_log10() +
+  facet_wrap(~ continent, ncol = 4)
+
+library(MetBrewer)
+ggplot(gapminder,
+       aes(y = lifeExp, x = pop, size = gdpPercap, color = continent)) +
+  geom_point(alpha = 0.6) +
+  scale_x_log10() +
+  scale_colour_manual(values = met.brewer("Klimt")) +
+  facet_wrap(~ year)
+
+# facet_grid()
+ggplot(gapminder %>% filter(year %in% c(1952,  1972, 1982,  2002)),
+       aes(y = gdpPercap, x = lifeExp)) +
+  geom_point(alpha = 0.9) +
+  scale_y_log10() +
+  facet_grid(year ~ continent)
+
+### Coordenadas y tema
+
+ggplot(gapminder_1997,
+       aes(y = gdpPercap, x = lifeExp,
+           color = continent, size = pop)) +
+  geom_point(alpha = 0.8) +
+  # Eje Y con escala logarítmica
+  scale_y_log10() +
+  scale_color_tableau() +
+  labs(title = "EJEMPLO DE SCATTERPLOT CON GGPLOT2",
+       subtitle =
+         "Esperanza vida vs renta per cápita (año 1997)",
+       caption = "Autor: Javier Álvarez Liébana | Datos: gapminder")
+
+ggplot(gapminder_1997,
+       aes(y = gdpPercap, x = lifeExp,
+           color = continent, size = pop)) +
+  geom_point(alpha = 0.8) +
+  # Eje Y con escala logarítmica
+  scale_y_log10() +
+  scale_color_tableau() +
+  labs(x = "Esperanza de vida", y = "Renta per cápita",
+       color = "Continente", size = "Población",
+       title = "EJEMPLO DE SCATTERPLOT CON GGPLOT2",
+       subtitle =
+         "Esperanza vida vs renta per cápita (año 1997)",
+       caption = "Autor: Javier Álvarez Liébana | Datos: gapminder")
+
+ggplot(gapminder_1997,
+       aes(y = gdpPercap, x = lifeExp,
+           color = continent, size = pop)) +
+  geom_point(alpha = 0.8) +
+  # Eje Y con escala logarítmica
+  scale_y_log10() +
+  scale_color_tableau() +
+  labs(x = "Esperanza de vida",
+       y = "Renta per cápita",
+       color = NULL, size = "Población",
+       title = "EJEMPLO DE SCATTERPLOT CON GGPLOT2",
+       subtitle =
+         "Esperanza vida vs renta per cápita (año 1997)",
+       caption = "Autor: Javier Álvarez Liébana | Datos: gapminder")
+
+# ocultar la leyenda
+ggplot(gapminder_1997,
+       aes(y = gdpPercap, x = lifeExp,
+           color = continent, size = pop)) +
+  geom_point(alpha = 0.8) +
+  # Eje Y con escala logarítmica
+  scale_y_log10() +
+  scale_color_tableau() +
+  guides(size = "none") +
+  labs(x = "Esperanza de vida",
+       y = "Renta per cápita",
+       color = NULL, size = "Población",
+       title = "EJEMPLO DE SCATTERPLOT CON GGPLOT2",
+       subtitle =
+         "Esperanza vida vs renta per cápita (año 1997)",
+       caption = "Autor: Javier Álvarez Liébana | Datos: gapminder")
+
+ggplot(gapminder_1997,
+       aes(y = gdpPercap, x = lifeExp,
+           color = continent, size = pop)) +
+  geom_point(alpha = 0.8) +
+  # Eje Y con escala logarítmica
+  scale_y_log10() +
+  scale_color_tableau() +
+  guides(size = "none", color = "none") +
+  labs(x = "Esperanza de vida",
+       y = "Renta per cápita",
+       color = NULL, size = "Población",
+       title = "EJEMPLO DE SCATTERPLOT CON GGPLOT2",
+       subtitle =
+         "Esperanza vida vs renta per cápita (año 1997)",
+       caption = "Autor: Javier Álvarez Liébana | Datos: gapminder")
+
+## Segundo intento: diagrama de barras
+
+# geom_col()
+ggplot(gapminder,
+       aes(y = gdpPercap, x = year)) +
+  geom_col()
+
+gapminder_por_continente <-
+  gapminder %>% group_by(year, continent) %>%
+  summarise(sum_gdpPercap = sum(gdpPercap))
+gapminder_por_continente
+
+ggplot(gapminder_por_continente,
+       aes(y = sum_gdpPercap, x = year,
+           fill = continent)) +
+  geom_col() +
+  scale_fill_tableau()
+
+ggplot(gapminder_por_continente,
+       aes(y = sum_gdpPercap, x = year,
+           fill = continent)) +
+  geom_col(position = "dodge2") +
+  scale_fill_tableau()
+
+ggplot(gapminder_por_continente,
+       aes(y = sum_gdpPercap, x = year,
+           fill = continent)) +
+  geom_col(position = "fill") +
+  scale_fill_tableau()
+
+# horizontales
+ggplot(gapminder_por_continente,
+       aes(y = sum_gdpPercap, x = year,
+           fill = continent)) +
+  geom_col() + coord_flip() +
+  scale_fill_tableau() +
+  labs(x = "Renta per cápita",
+       y = "Año", color = "Continente",
+       title = "EJEMPLO DE DIAGRAMA DE BARRAS CON GGPLOT2",
+       subtitle =
+         "Barras horizontales apiladas (agrupadas por continente y año)",
+       caption = "Autor: Javier Álvarez Liébana | Datos: gapminder")
+
+
